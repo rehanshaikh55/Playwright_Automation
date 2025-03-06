694ac6a4b0536e7c37c518cad08704508bf0c417
rehanshaikh55 <shaikhrehan1016@gmail.com>
2025-03-06 18:33:30 +0530

    a

--- tests/salWebLinks.spec.js
+++ tests/salWebLinks.spec.js
@@ -3,7 +3,7 @@ import {test,expect} from '@playwright/test'
 test('Sal website links',async({page})=>{
      await  page.goto("https://www.sal.edu.in/")
      const links = await page.$$eval('a', anchors => anchors.map(a => a.href));
-
-     // Print each link
+     
+     //// Print each link
      console.log('Collected Links:', links);
 }) 
