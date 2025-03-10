1eb3838effe184ab24c8b32665628ca68b3424a8
rehanshaikh55 <shaikhrehan1016@gmail.com>
2025-03-10 17:27:34 +0530

    redbus example

---
+++ tests/redBus.spec.js
@@ -0,0 +1,19 @@
+import {test} from '@playwright/test'
+
+test('auto suggestion box in red bus', async ({page}) => { 
+
+  await page.goto('https://www.redbus.in/')
+  await page.fill("//input[@id='src']",'ahmedabad')
+  await page.waitForSelector("//li[contains(@class,'sc-iwsKbI')]/div/text[1]")
+
+  const options = await page.$$("//li[contains(@class,'sc-iwsKbI')]/div/text[1]")
+  for (let op of options){
+    const value = await op.textContent();
+    console.log(value);
+    if(value.includes === "Satellite"){
+        op.click();
+    }
+    
+  }
+await page.waitForTimeout(3000)
+ })
