1101c18896feba5604875b279082fd80e224aede
rehanshaikh55 <shaikhrehan1016@gmail.com>
2025-03-12 16:13:48 +0530

    .

--- tests/iFrames.spec.js
+++ tests/iFrames.spec.js
@@ -4,7 +4,7 @@ test('iFrame handle', async ({page}) => {
 
 
    await page.goto('https://ui.vision/demo/webtest/frames/')
-   //const frames = page.frames()
+   //const frames = awaits page.frames()
    //console.log(frames.length);
    const frame1 = await page.frame({url:"https://ui.vision/demo/webtest/frames/frame_1.html"})
    await frame1.fill("//input[@name='mytext1']","rehan")
