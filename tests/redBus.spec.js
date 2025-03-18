b80262effb5333e6e8a6560c2dac2312c05e2bdd
rehanshaikh55 <shaikhrehan1016@gmail.com>
2025-03-18 17:40:30 +0530

    fix: correct screenshot capture and improve auto-suggestion handling in redBus test

--- tests/redBus.spec.js
+++ tests/redBus.spec.js
@@ -2,16 +2,16 @@ import {test} from '@playwright/test'
 
 test('auto suggestion box in red bus', async ({page}) => { 
 
-  await page.goto('https://www.redbus.in/')
+  await page.goto('https://www.redbus.in/');
   await page.fill("//input[@id='src']",'ahmedabad')
   await page.waitForSelector("//li[contains(@class,'sc-iwsKbI')]/div/text[1]")
-  await page .screenshot({path:"tests/upload/"+Date.now() +"redbus.png",fullPage:true})
+  await page.screenshot({path:"tests/upload/"+Date.now() +"redbus.png",fullPage:true})
   const options = await page.$$("//li[contains(@class,'sc-iwsKbI')]/div/text[1]")
   for (let op of options){
     const value = await op.textContent();
     console.log(value);
-    if(value.includes === "Ahmedabad"){
-        op.click();
+    if(value.includes("Ahmedabad")){
+        await op.click();
     }
      
   }
